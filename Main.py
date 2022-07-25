a3c58810882953b7982fe33cfbee3fb5009c2d9f
JasonJacob07 <100529935+JasonJacob07@users.noreply.github.com>
2022-07-25 17:40:17 +0530

    initialcommit

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-"""Importing operating system module"""
 import os
 
 class Stack:
@@ -6,41 +5,35 @@ class Stack:
     def __init__(self, size):
         self.items = []
         self.size = size
-
-"""This function is used to return if the stack is empty"""
+        
     def is_empty(self):
         if len(self.items) == 0:
             return True
         else:
           return False
         
-"""This function is used to return if the stack is full"""
     def is_full(self):
         if len(self.items) == self.size:
             return True
         else:
             return False
           
-"""function to push data into the stack"""
     def push(self, data):
         if not self.is_full():
             self.items.append(data)
 
-""" function to pop elements in the stack from top"""
     def pop(self):
         if not self.is_empty():
             # Write code here
             self.items.pop()
-
-"""to display all the elements in the stack"""         
+            
     def status(self):
         for element in self.items:
             print(element)
           
-"""inputting size and query of the stack"""
+
 size, queries = map(int, input().rstrip().split())
 
-"""creating object module"""
 stack = Stack(size)
 for line in range(queries):
     values = list(map(int, input().rstrip().split()))
